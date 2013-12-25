89511335a69065bba2008bfe89f1e73adeead5dd
Giles Lavelle <giles.lavelle@gmail.com>
2013-12-25 19:58:12 +0000

    tidy require config

--- js/requireconf.js
+++ js/requireconf.js
@@ -3,46 +3,22 @@ require.config({
 
     paths: {
         'jquery': 'vendor/jquery/jquery',
-
-        'entity': 'entity',
-        'npc': 'npc',
-
-        'player': 'player',
-        'creature': 'creature',
-        'lumberjack': 'lumberjack',
-
-        'game': 'game',
-        'scene': 'scene',
-        'renderer': 'renderer',
-
-        'plugins': 'plugins',
         'keymaster': 'vendor/keymaster/keymaster',
-
         'underscore': 'vendor/underscore/underscore',
         'pathfinding': 'vendor/PathFinding.js/lib/pathfinding-browser',
         'noise': 'vendor/noisejs/index',
-
         'stats': 'vendor/stats.js/src/Stats'
     },
 
     shim: {
-        'keymaster': {
+        keymaster: {
             exports: 'key'
         },
-
-        'underscore': {
+        underscore: {
             exports: '_'
         },
-
-        'stats': {
+        stats: {
             exports: 'Stats'
         }
-
-        // 'backbone': {
-        //     deps: ['underscore', 'jquery'],
-        //     exports: 'Backbone'
-        // },
-
-        // 'layoutmanager': ['jquery', 'underscore', 'backbone'],
     }
 });
